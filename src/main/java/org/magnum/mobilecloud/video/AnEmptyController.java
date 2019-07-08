ddf2c13c72ecaaeefbf522caa6416b9d8ed6354c
JulianDeng <dzhongr@my.yorku.ca>
2019-07-07 22:47:18 -0400

    1st commit

--- src/main/java/org/magnum/mobilecloud/video/AnEmptyController.java
+++ src/main/java/org/magnum/mobilecloud/video/AnEmptyController.java
@@ -18,10 +18,21 @@
 
 package org.magnum.mobilecloud.video;
 
+import java.security.Principal;
+import java.util.Collection;
+
+import org.magnum.mobilecloud.video.client.VideoSvcApi;
+import org.magnum.mobilecloud.video.repository.Video;
+import org.magnum.mobilecloud.video.repository.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.Lists;
 
 @Controller
 public class AnEmptyController {
@@ -42,10 +53,33 @@ public class AnEmptyController {
                                                                                                                                                                                                                                                                         
 	 * 
 	 */
+	@Autowired
+	private VideoRepository videos;
+	
 	
 	@RequestMapping(value="/go",method=RequestMethod.GET)
 	public @ResponseBody String goodLuck(){
 		return "Good Luck!";
 	}
 	
+	@RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.GET)
+	public @ResponseBody Collection<Video> getAllVideos(Principal p){
+		return Lists.newArrayList(videos.findAll());
+	}
+	
+	@RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.POST)
+	public @ResponseBody Video addVideo(@RequestBody Video v, Principal p){
+		videos.save(v);
+		return v;
+	}
+	
+	@RequestMapping(value=VideoSvcApi.VIDEO_TITLE_SEARCH_PATH, method=RequestMethod.GET)
+	public @ResponseBody Collection<Video> findByTitle(@RequestParam(VideoSvcApi.TITLE_PARAMETER) String title, Principal p){
+		return videos.findByName(title);
+	}
+	
+	@RequestMapping(value=VideoSvcApi.VIDEO_DURATION_SEARCH_PATH, method=RequestMethod.GET)
+	public @ResponseBody Collection<Video> findByDurationLessThan(@RequestParam(VideoSvcApi.DURATION_PARAMETER) long duration, Principal p){
+		return videos.findByDurationLessThan(duration);
+	}
 }
